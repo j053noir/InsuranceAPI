e66dc210ce691449cdb2c03bc67a18d5b41d9a9d
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2023-07-29 18:30:15 -0500

    Configured database instance for DI

--- InsuranceAPI/Program.cs
+++ InsuranceAPI/Program.cs
@@ -1,6 +1,25 @@
+using InsuranceAPI.Infrastructure.Models;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
 
 // Add services to the container.
+
+#region Configure Database
+string databaseConnectionString = configuration.GetConnectionString("MongoDatabase");
+
+var client = new MongoClient(databaseConnectionString);
+
+string databaseName = configuration["MongoDatabase:DatabaseName"];
+
+IMongoDatabase database = client.GetDatabase(databaseName);
+
+builder.Services.AddSingleton(database);
+
+builder.Services.Configure<InsuranceAPI.Infrastructure.Models.MongoDatabaseSettings>
+                                        (builder.Configuration.GetSection("MongoDatabase"));
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
