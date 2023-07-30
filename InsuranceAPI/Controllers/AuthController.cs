0e221de6a4b3b52c4ab43c3259a69543ab5ba62a
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2023-07-30 10:40:50 -0500

    Created Auth and User controllers

---
+++ InsuranceAPI/Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using InsuranceAPI.Infrastructure.DTOs.Users;
+using InsuranceAPI.Infrastructure.Services.Interfaces;
+using InsuranceAPI.Utils.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IUserService _userService;
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(ILogger<AuthController> logger, IMapper mapper, IUserService userService, IAuthenticationService authenticationService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userService = userService;
+            _authenticationService = authenticationService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(AuthenticationRequestDTO model)
+        {
+            var ipAddress = GetIPAddress();
+            var response = await _userService.Authenticate(model, ipAddress);
+            return Json(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegistrationRequestDTO model)
+        {
+            model.Role = Infrastructure.Models.UserRole.Client;
+            await _userService.Register(model);
+
+            var authenticateModel = _mapper.Map<AuthenticationRequestDTO>(model);
+
+            var ipAddress = GetIPAddress();
+            var response = await _userService.Authenticate(authenticateModel, ipAddress);
+
+            return Json(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(RefreshTokenRequestDTO model)
+        {
+            var token = model.Token;
+
+            if (string.IsNullOrEmpty(token))
+                return BadRequest(new { message = "Token is required" });
+
+            var response = await _userService.RefreshToken(token, GetIPAddress());
+            return Json(response);
+        }
+
+        [HttpPost("revoke-token")]
+        public async Task<IActionResult> RevokeToken(RevokeRefreshTokenDTO model)
+        {
+            var token = model.Token;
+
+            if (string.IsNullOrEmpty(token))
+                return BadRequest(new { message = "Token is required" });
+
+             await _userService.RevokeToken(token, GetIPAddress());
+            return Json(new { message = "Token revoked"});
+        }
+
+        private string GetIPAddress()
+        {
+            var ipAddress = HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+
+            if (string.IsNullOrEmpty(ipAddress)) throw new ApplicationException("IP Address cannot be found");
+
+            return ipAddress;
+        }
+    }
+}
---
+++ InsuranceAPI/Controllers/UserController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using InsuranceAPI.Infrastructure.DTOs.Users;
+using InsuranceAPI.Infrastructure.Models;
+using InsuranceAPI.Infrastructure.Services.Interfaces;
+using InsuranceAPI.Utils.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace InsuranceAPI.Controllers
+{
+    [Authorize(UserRole.Administrator)]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(ILogger<UserController> logger, IMapper mapper, IUserService userService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+
+            var response = _mapper.Map<IEnumerable<UserReponseDTO>>(users);
+
+            return Json(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var user = await _userService.GetById(new ObjectId(id));
+
+            var response = _mapper.Map<UserReponseDTO>(user);
+
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegistrationRequestDTO model)
+        {
+            await _userService.Register(model);
+            return Json(new { message = "User created successfully" });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromBody] UpdateRequestDTO model)
+        {
+            await _userService.Update(new ObjectId(id), model);
+            return Json(new { message = "User updated successfully" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _userService.Delete(new ObjectId(id));
+            return Json(new { message = "User deleted successfully" });
+        }
+
+        [HttpGet("user-roles")]
+        public IActionResult GetAllUserRoles()
+        {
+            var userRoles = new List<UserRoleResponseDTO>();
+
+            var roles = Enum.GetValues<UserRole>();
+
+            for (int i = 0; i < roles.Length; i++)
+            {
+                userRoles.Add(new UserRoleResponseDTO
+                {
+                    Name = roles[i].ToString(),
+                    Value = (int)roles[i]
+                });
+            }
+
+            return Json(userRoles);
+        }
+    }
+}
